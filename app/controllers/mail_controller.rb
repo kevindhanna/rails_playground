0fa553d95663210ef76e4830bab005d1c21baf12
Kevin Hanna <kevindhanna@live.com>
2021-10-04 19:30:51 +0930

    sigh more

--- app/controllers/mail_controller.rb
+++ app/controllers/mail_controller.rb
@@ -2,13 +2,14 @@ class MailController < Griddler::EmailsController
   before_action :do_stuff
 
   def do_stuff
-    File.open('raw', 'w') { |f|
-      f.write(request.raw_post)
+    raw_request = request.raw_post
+    File.open('./raw', 'w') { |f|
+      f.write(raw_request)
     }
-    File.open('json', 'w') { |f|
+    File.open('./json', 'w') { |f|
       f.write(request.as_json)
     }
-    File.open('params', 'w') { |f|
+    File.open('./params', 'w') { |f|
       f.write(params.as_json)
     }
   end
