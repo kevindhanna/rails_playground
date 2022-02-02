e6a35f5b65c5a08b89551739d44f41e9f74001b6
Kevin Hanna <kevindhanna@fastmail.com>
2022-02-02 12:27:56 +0000

    add location

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   validates :name, presence: true
   validates :email, format: /@/, presence: true, uniqueness: { case_sensitive: false }
+  belongs_to :location
 end
